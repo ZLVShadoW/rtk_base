596dca79a676f5d6dd256f1c05fc492a5ebb04c6
ZLVShadoW <zlvshadow@gmail.com>
2022-10-27 02:25:49 +0300

    fix/add: routes/error page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import '../App.css';
 import {Layout} from '../Pages/Layout';
 import {Main} from '../Pages/Main';
 import {Pokemons} from '../Pages/Pokemons';
 import {Counters} from '../Pages/Counters';
 import { Pokemon } from '../Pages/Pokemon';
+import {Error} from '../Pages/Error';
 
 export const App = () => {
     return (
@@ -15,6 +16,8 @@ export const App = () => {
                 <Route path={'counters'} element={<Counters/>}/>
                 <Route path={'pokemons'} element={<Pokemons/>}/>
                 <Route path={'pokemons/:idp'} element={<Pokemon/>}/>
+                <Route path={'404'} element={<Error/>}/>
+                <Route path={'*'} element={<Navigate to={'404'} />}/>
             </Route>
         </Routes>
     );
