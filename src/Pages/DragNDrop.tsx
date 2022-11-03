61e44803685ebb3afed7c7ab52a40ae4ab2156ca
ZLVShadoW <zlvshadow@gmail.com>
2022-11-04 01:19:13 +0300

    practice: drag and drop first steps

--- src/Pages/DragNDrop.tsx
+++ src/Pages/DragNDrop.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
 export const DragNDrop = () => {
+let dr: Element
+    const onDragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        dr = e.currentTarget
+    }
+
+    const onDragEndHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+    }
+
+    const onDragOverHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        e.currentTarget.style.background = '#ccc'
+    }
+
+    const onDropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        e.currentTarget.appendChild(dr)
+        e.currentTarget.style.background = '#eee'
+        dr.setAttribute('draggable', 'false')
+    }
+
     return (
-        <div>
-            DragNDrop
-        </div>
+        <>
+            <div style={{background: '#eee', padding: 10, margin: 10}}>
+                <div onDragStart={onDragStartHandler}
+                     onDragEnd={onDragEndHandler}
+                     draggable={true}
+                >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing.
+                </div>
+            </div>
+
+            <div style={{background: '#eee', padding: 30, margin: 10}}
+                 onDragOver={onDragOverHandler}
+                 onDrop={onDropHandler}
+            >
+                zcxzczc
+            </div>
+        </>
     );
 };
